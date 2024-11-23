a95aa72653cb4afd9d4763c14e375991b1ddb17e
Mayass-Ben-Mammar <benmammar.mayass@gmail.com>
2024-11-23 16:42:19 +0100

    Ajout menu pause
    Ajout du menu pour mettre le jeu en pause

---
+++ Code/Pause.gd
@@ -0,0 +1,21 @@
+extends Control
+
+var est_en_pause = false setget en_pause
+
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		self.est_en_pause = !est_en_pause
+
+
+func en_pause(val):
+	est_en_pause = val
+	get_tree().paused = est_en_pause
+	visible = est_en_pause
+
+
+func _on_Continuer_pressed():
+	self.est_en_pause = false
+
+
+func _on_Quitter_pressed():
+	get_tree().quit()
