7f6730d73d043adbc08a1e843a427118c99779e0
Mayass-Ben-Mammar <benmammar.mayass@gmail.com>
2024-11-28 15:36:20 +0100

    Selection avec touche
    Maintenant lorsque le joueur appuis sur un touche il active le bouton pour lancer le jeu

--- Code/PlayButton.gd
+++ Code/PlayButton.gd
@@ -3,3 +3,7 @@ extends Button
 func _on_PlayButton_pressed():
 	Global.lives = Global.max_lives
 	get_tree().change_scene("res://Scene/Niveau 0.tscn")
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("jump"):
+		_on_PlayButton_pressed()
