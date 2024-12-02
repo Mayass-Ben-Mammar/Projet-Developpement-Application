8dbed736520114af6cc62a6f3c2e1671cb5d6ee2
Mayass-Ben-Mammar <benmammar.mayass@gmail.com>
2024-12-02 16:23:57 +0100

    Mise a jour du dictionnaire et ajout de niveau 2

--- Code/Global.gd
+++ Code/Global.gd
@@ -29,7 +29,10 @@ func charger():
 	if not fichier.file_exists(SAVE_FILE):
 		J_donnees = {
 			"premiere_fois": true,
-			"Fleur11": 0
+			"N11": 0,  "N12": 0,  "N13": 0,  "N14": 0,  "N15": 0,  "N16": 0,  "N17": 0,  "N18": 0,  "N19": 0,  "N110": 0,
+			"Fleur11": 0,  "Fleur12": 0,  "Fleur13": 0,  "Fleur14": 0,  "Fleur15": 0,  "Fleur16": 0,  "Fleur17": 0,
+			"Fleur18": 0,  "Fleur19": 0,  "Fleur110": 0,  "Fleur111": 0,  "Fleur112": 0,  "Fleur113": 0,  "Fleur114": 0,
+			"Fleur115": 0,
 		}
 		sauvegarder()
 	else:
--- Code/Monde 1/Niveau 2/Niveau 2-0.gd
+++ Code/Monde 1/Niveau 2/Niveau 2-0.gd
@@ -5,12 +5,10 @@ onready var save = Global.J_donnees
 func _ready():
 	$"Test Perso/Camera2D".limit_top = -64
 	$"Test Perso/Camera2D".limit_right = 960*3
-	$GUI/HUD/Fleur2.hide()
 	$GUI/HUD/Fleur3.hide()
-	if save.Fleur11 == 1:
+	if save.Fleur12 == 1:
 		$Fleur.queue_free()
 		$GUI/HUD/Fleur1A.show()
-
-func _on_Fleur_FleurRecuperer(val):
-	save.Fleur11 = 1
-	Global.sauvegarder()
+	elif save.Fleur13 == 1:
+		$Fleur.queue_free()
+		$GUI/HUD/Fleur2A.show()
