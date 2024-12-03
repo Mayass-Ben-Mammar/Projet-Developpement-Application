fb65d21dd206800b658086597b9096130eb48e4b
Mayass-Ben-Mammar <benmammar.mayass@gmail.com>
2024-12-03 19:04:43 +0100

    Finition du niveau 4 monde 1

---
+++ Code/Monde 1/Niveau 4/N4VH.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+onready var save = Global.J_donnees
+var entree = false
+
+func _on_N4VH_body_entered(body : PhysicsBody2D):
+	entree = true
+
+func _on_N4VH_body_exited(body):
+	entree = false
+
+func _process(delta):
+	if entree == true:
+		if Input.is_action_just_pressed("up"):
+			save.N14 = 1
+			Global.sauvegarder()
+			Transition.Change_Niveau("res://Scene/Monde 1/HUB1.tscn")
+
--- Code/Monde 1/Niveau 4/Niveau 4-1.gd
+++ Code/Monde 1/Niveau 4/Niveau 4-1.gd
@@ -3,16 +3,15 @@ extends Node2D
 onready var save = Global.J_donnees
 
 func _ready():
-	$"Test Perso/Camera2D".limit_top = -7*32
-	$"Test Perso/Camera2D".limit_right = 160*32
+	$"Test Perso/Camera2D".limit_top = -48*32
+	$"Test Perso/Camera2D".limit_right = 960
 	$GUI/HUD/Fleur3.hide()
 	if save.Fleur15 == 1:
-		$Fleur.queue_free()
 		$GUI/HUD/Fleur1A.show()
 	if save.Fleur16 == 1:
 		$GUI/HUD/Fleur2A.show()
 
 
-func _on_Fleur_FleurRecuperer(val):
-	save.Fleur15 = 1
+func _on_Fleur_FleurRecuperer():
+	save.Fleur16 = 1
 	Global.sauvegarder()
