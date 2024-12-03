c5d0d369e79a2be663334b20dda9f124dc74ea2f
Mayass-Ben-Mammar <benmammar.mayass@gmail.com>
2024-12-03 22:53:02 +0100

    mise à jour de la map

--- Code/MAP.gd
+++ Code/MAP.gd
@@ -14,10 +14,10 @@ func _physics_process(delta):
 	match monde_actuelle:
 		1:
 			$CanvasLayer/Fleche1.show()
-			if Input.is_action_just_pressed("right") or Input.is_action_just_pressed("down"):
+			if (Input.is_action_just_pressed("right") or Input.is_action_just_pressed("down"))and save.M2 == 1:
 				monde_actuelle = 2
 				$CanvasLayer/Fleche1.hide()
-			elif Input.is_action_just_pressed("up"):
+			elif Input.is_action_just_pressed("up") and save.M5 == 1:
 				monde_actuelle = 5
 				$CanvasLayer/Fleche1.hide()
 			if Input.is_action_just_pressed("jump") and save.premiere_fois == false:
@@ -28,7 +28,7 @@ func _physics_process(delta):
 				Transition.Change_Niveau("res://Scene/Monde 1/Niveau 1/Niveau 1-0.tscn")
 		2:
 			$CanvasLayer/Fleche2.show()
-			if Input.is_action_just_pressed("right"):
+			if Input.is_action_just_pressed("right") and save.M3 == 1:
 				monde_actuelle = 3
 				$CanvasLayer/Fleche2.hide()
 			elif Input.is_action_just_pressed("left") or Input.is_action_just_pressed("up"):
@@ -36,18 +36,18 @@ func _physics_process(delta):
 				$CanvasLayer/Fleche2.hide()
 		3:
 			$CanvasLayer/Fleche3.show()
-			if Input.is_action_just_pressed("right") or Input.is_action_just_pressed("up"):
+			if (Input.is_action_just_pressed("right") or Input.is_action_just_pressed("up")) and save.M4 == 1:
 				monde_actuelle = 4
 				$CanvasLayer/Fleche3.hide()
-			elif Input.is_action_just_pressed("left"):
+			elif Input.is_action_just_pressed("left") and save.M2 == 1:
 				monde_actuelle = 2
 				$CanvasLayer/Fleche3.hide()
 		4:
 			$CanvasLayer/Fleche4.show()
-			if Input.is_action_just_pressed("left"):
+			if Input.is_action_just_pressed("left") and save.M5 == 1:
 				monde_actuelle = 5
 				$CanvasLayer/Fleche4.hide()
-			elif Input.is_action_just_pressed("down"):
+			elif Input.is_action_just_pressed("down") and save.M3 == 1:
 				monde_actuelle = 3
 				$CanvasLayer/Fleche4.hide()
 		5:
@@ -55,7 +55,7 @@ func _physics_process(delta):
 			if Input.is_action_just_pressed("left"):
 				monde_actuelle = 1
 				$CanvasLayer/Fleche5.hide()
-			elif Input.is_action_just_pressed("right"):
+			elif Input.is_action_just_pressed("right") and save.M4 == 1:
 				monde_actuelle = 4
 				$CanvasLayer/Fleche5.hide()
 
