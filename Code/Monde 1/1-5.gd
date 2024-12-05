09e1aa6fc82f0dc7c1aeb2eaeb386a448e5ba749
Mayass-Ben-Mammar <benmammar.mayass@gmail.com>
2024-12-05 21:42:57 +0100

    Ajout de porte dans le HUB du Monde 1

---
+++ Code/Monde 1/1-5.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+onready var save = Global.J_donnees
+var entree15 = false
+
+func _on_12_body_entered(body: PhysicsBody2D):
+	entree15 = true
+
+func _on_12_body_exited(body):
+	entree15 = false
+
+func _process(delta):
+	if entree15 == true and save.N14 == 1:
+		if Input.is_action_just_pressed("up"):
+			Transition.Change_Niveau("res://Scene/Monde 1/Niveau 5/Niveau 5-0.tscn")
+
